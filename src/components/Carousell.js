7888f700ff0b9741d79f8db5a92e83c22cb55cd5
atul-code-coder <atulpatel22777@gmail.com>
2023-02-22 18:15:55 +0530

    dashboard pic changed

--- src/components/Carousell.js
+++ src/components/Carousell.js
@@ -7,7 +7,7 @@ import "../asserts/scss/Carousell.scss"
 const Carousell = () => {
   return (
     <div className='container carousel-img'>
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" interval= "1000">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" interval="1000" >
         <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
